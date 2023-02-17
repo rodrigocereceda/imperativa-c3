2df5d367ff0ea54f2fd4dc783387eca4ea7cc44c
jorgeduje <jorgeduje4@gmail.com>
2023-02-17 20:10:10 -0300

    se trabajo con variables y tipos de datos

--- variables.js
+++ variables.js
@@ -1,2 +1,61 @@
 
-let nombre = "pepito"
+// ---> Variables -- una caja donde guardamos algo
+
+let nombreUsuario = "pepito"
+nombreUsuario = "juan"
+
+
+const apellido = "Perez"
+
+let edad = 30
+    edad = 31
+
+    // apellido = "otra cosa" // ESTO NO SE PUEDE
+
+// ---> TIPOS DE DATOS
+
+// string ---> cadenas de texto o caracteres
+let frase = "hola mundo" // ''
+
+
+// number
+let cantidadMascotas = 4
+    cantidadMascotas = cantidadMascotas + 1
+
+// console.log(precio) // NO SE PUEDE 
+let precio = 5.40
+
+// Booleanos 
+
+let isLogged = true // true = verdadero ||| false = falso
+let estaCasado = false
+let esMayorDeEdad = false
+
+// UNDEFINED
+
+// let email = undefined
+
+let email
+// console.log(email)
+
+// NULL 
+// NaN
+
+
+// console.log(nombreUsuario)
+
+// LAS VARIABLES SE INICIALIZAN UNA SOLA VEZ
+
+let password = "juan"
+
+console.log(password)
+
+password = true
+
+
+console.log(password)
+
+
+password = "hola"
+
+console.log(password)
